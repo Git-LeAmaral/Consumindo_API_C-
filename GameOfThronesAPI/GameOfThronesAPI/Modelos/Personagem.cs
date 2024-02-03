8cb204d10a7b513ede632d6da94264e3c0bc3d59
Leandro Amaral <lramaral02m@gmail.com>
2024-02-03 12:43:40 -0300

    Consumindo API de Game of Thrones

---
+++ GameOfThronesAPI/GameOfThronesAPI/Modelos/Personagem.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace GameOfThronesAPI.Modelos;
+
+internal class Personagem
+{
+    [JsonPropertyName("name")]
+    public string NomePersonagem { get; set; }
+
+    [JsonPropertyName("gender")]
+    public string Genero { get; set; }
+
+    [JsonPropertyName("culture")]
+    public string Civilizacao { get; set; }
+
+    [JsonPropertyName("born")]
+    public string Nascimento { get; set; }
+
+    [JsonPropertyName("aliases")]
+    public List<string>? Apelido { get; set; }
+
+    public void ExibirDadosDoPersonagem()
+    {
+        Console.WriteLine("====== FICHA DO PERSONAGEM ======");
+        Console.WriteLine($"Nome: {NomePersonagem}");
+        Console.WriteLine($"Genero: {Genero}");
+        Console.WriteLine($"Civilização: {Civilizacao}");
+        Console.WriteLine($"Nasceu em: {Nascimento}");
+
+        foreach (string apelido in Apelido)
+        {
+            Console.WriteLine($"Apelido: {apelido}");
+        }
+        Console.WriteLine("===============================");
+    }
+}
---
+++ GameOfThronesAPI/GameOfThronesAPI/Program.cs
@@ -0,0 +1,18 @@
+﻿using GameOfThronesAPI.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://www.anapioficeandfire.com/api/characters/685");
+        //Console.WriteLine(resposta);
+
+        Personagem personagem = JsonSerializer.Deserialize<Personagem>(resposta)!;
+        personagem.ExibirDadosDoPersonagem();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Opps! Tem algo errado: {ex.Message}");
+    }
+}
